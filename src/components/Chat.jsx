12efab67ce82a7984702c73397d28ad3cdca116b
Егор Филиппов <egorfilippov@MacBook-Air-Egor.local>
2023-06-10 23:23:44 +0400

    new link

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import styles from '../styles/Chat.module.css';
 import Messages from "./Messages";
 
-const socket = io.connect('http://localhost:5500');
+const socket = io.connect('https://practice-10variant-fil-eg.onrender.com');
 
 const Chat = () => {
     const { search } = useLocation();
